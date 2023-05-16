913c96d2bdcee0f598cc79f9b6ee7337ba109528
Simone Dos Santos Silva <wirus1@gmail.com>
2023-05-16 15:44:45 -0300

    API Rest
    API com cypress

---
+++ cypress/contracts/produtos.contract.js
@@ -0,0 +1,13 @@
+const Joi = require ('joi') // constante que chama o joi
+
+const produtosSchema = Joi.object({ // constante que contem os parametros dos produtos
+ quantidade: Joi.number(),
+ produtos: Joi.array().items({
+    nome: Joi.string(),
+    preco: Joi.number(),
+    descricao: Joi.string(),
+    _id: Joi.string()
+ })
+})
+
+export default produtosSchema; // exportar
